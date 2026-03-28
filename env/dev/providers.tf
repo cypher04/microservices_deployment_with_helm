5d33390429005a69925215643c83d4f4dc660254
Seyi Abiodun <cypher04>
2026-03-28 14:43:38 +0000

    refactor(provider.tf, values.yaml, service.yaml): Refactored helm provider to reference AKS attributes, also passed kubernetes attributes as arguments due to v3 changes. chnged number of replicaCount, tag and image in helm "values.yaml" file. Added namespace reference code to service.yaml file.

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -34,11 +34,11 @@ provider "kubernetes" {
 
 // helm provider configuration
 provider "helm" {
-  kubernetes {
-    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate) 
+  kubernetes = {
+    host                   = module.aks.host
+    client_certificate     = base64decode(module.aks.client_certificate)
+    client_key             = base64decode(module.aks.client_key)
+    cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate) 
     }
 }
 
