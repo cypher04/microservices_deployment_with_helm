93c8a471e34740f70558950e7820fd6c01806243
Seyi Abiodun <cypher04>
2026-04-07 14:24:08 +0100

    feature(managed database): Deployed managed postgresql database.

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -9,7 +9,18 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
   storage_tier = "P4"
   sku_name                     = "B_Standard_B1ms"
   delegated_subnet_id           = var.subnet_ids["database"]
-zone = "1"
+  zone = "2"
+  private_dns_zone_id = var.private_dns_zone_vl_id
+  public_network_access_enabled = false
 
+
+depends_on = [ var.private_dns_zone_vl_id ]
   
+}
+
+resource "azurerm_postgresql_flexible_server_database" "postgreql_server" {
+  name                = "myappdb"
+  server_id         = azurerm_postgresql_flexible_server.postgresql.id
+  charset             = "UTF8"
+  collation           = "en_US.utf8"
 }
